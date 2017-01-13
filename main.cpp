761b32eea67250fe34a6b4151fdb7934f5be73e5
Matthew Kidd <kidma@Kidma-2.local>
2017-01-13 13:38:27 -0700

    Added file read in. Uses vector.

--- main.cpp
+++ main.cpp
@@ -5,37 +5,45 @@
 
 int main() {
     std::ifstream fin;
-    fin.open ("../Data/Dictionary.txt");
-    std::vector <std::string> aList(fin);
-    while(fin) {
-        
+    fin.open ("Data/Dictionary.txt");
+    std::vector <std::string> aList;
+    std::string temp = "";
+
+    if(fin) {
+        int i = 0;
+        while(!fin.eof()) {
+            fin >> temp;
+            aList.push_back(temp);
+            i++;
+        }
+        std::cout << "\nFirst Word: " << aList.at(0)  << "\nLast Word: " << aList.at(aList.size()-1);
     }
     return 0;
 }
 
 class Node {
-    public: 
-        Node() {
-            m_word;
-            prev = nullptr;
-            next = nullptr;
-        }
-        void head() {
-            
-        }
-        void tail() {
-            
-        }
-    private:
-        std::string m_word;
-        Node* prev;
-        Node* next;
-        
+public:
+    Node() {
+        m_word;
+        prev = nullptr;
+        next = nullptr;
+    }
+    void head() {
+
+    }
+    void tail() {
+
+    }
+private:
+    std::string m_word;
+    Node* prev;
+    Node* next;
+
 };
 
 class DoublyLinkedList {
-    public:
-        
-    private:
-        
+public:
+
+private:
+
 };
